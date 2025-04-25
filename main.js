31fa51197982479acde543d1f6ab7dda9db3c4ac
Dominic <dominicduf@gmail.com>
2025-04-25 17:56:53 -0400

    Create the playRound function
    Select if the possibility is a win, a loss or a
    tie then prints a message.

--- main.js
+++ main.js
@@ -9,4 +9,29 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Choose Rock, Paper Scissors !")
 }
-console.log(getHumanChoice())
+
+function playRound(hummanchoice,computerchoice) {
+    human = hummanchoice.toUpperCase()
+    computer = computerchoice.toUpperCase()
+
+    let states_dictionary={ 
+        "ROCK":{"ROCK":"TIE","PAPER":"LOSE","SCISSORS":"WIN"}, 
+        "PAPER":{"ROCK":"WIN","PAPER":"TIE","SCISSORS":"LOSE"}, 
+        "SCISSORS":{"ROCK":"LOSE","PAPER":"WIN","SCISSORS":"TIE"}
+    };
+
+    let game = states_dictionary[human][computer]
+
+    if (game == "TIE") {
+        return "It's a TIE, " + hummanchoice + " is the same has " + computerchoice
+    } else if (game == "WIN") {
+        return "You win, " + hummanchoice + " beats " + computerchoice
+    } else {
+        return "You Lose, " + hummanchoice + " loses against " + computerchoice
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection))
